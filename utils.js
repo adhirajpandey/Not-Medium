08f661b33c1d0b9ef6c4590cea7feda026ba7303
adhirajpandey <pandey.adhiraj02@gmail.com>
2024-02-22 08:13:23 +0530

    change fetch article logic

--- utils.js
+++ utils.js
@@ -1,13 +1,12 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
-const fs = require('fs')
 
 const baseCacheURL = "https://webcache.googleusercontent.com/search?q=cache:"
 
 async function unmediumify(mediumURL) {
     try {
         const finalURL = `${baseCacheURL}${mediumURL}&ie=UTF-8&strip=1&vwsrc=0`
-        console.log(finalURL)
+
         const response = await axios.get(finalURL)
     
         if (response.status !== 200) {
@@ -15,24 +14,14 @@ async function unmediumify(mediumURL) {
             return {title: "", htmlData: ""}
         }
 
-        const htmlContent = response.data
+        const $ = cheerio.load(response.data)
 
-        const $ = cheerio.load(htmlContent)
-
-        // Extract title
         const title = $('title').text()
 
-        // Fetch all tags with 'p' or 'h1' and preserve order
-        const tagContent = $("p, h1").map((_, element) => {
-        const tagName = $(element).prop("tagName").toLowerCase()
-        return `<${tagName}>${$(element).text()}</${tagName}>\n`
-        }).get().join('')
+        $("*").removeAttr("class")
 
-        const contentStartIndex = tagContent.indexOf("Share") + 10
-        const contentEndIndex = tagContent.indexOf("medium.com") - 4
-
-        const htmlData = tagContent.substring(contentStartIndex, contentEndIndex)
-
+        const htmlData = $("#root > div > div:nth-child(2) > div:nth-child(3) > div:nth-child(1)").html()
+        
         return {title, htmlData}
 
     } catch (error) {
